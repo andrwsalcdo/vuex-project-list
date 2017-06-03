af7737433ecd611cd49b3a913dad6e61233b3492
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-02 22:22:30 -0400

    add Containers. displays navbar and routes pages

---
+++ src/containers/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="dashboard">
+        Dashboard content will go here
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dashboard'
+}
+</script>
---
+++ src/containers/projects.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="projects">
+        projects will go here 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'projects'
+}
+</script>
