db72ba76d99a9f1654651910cd90d9893b6e3f3d
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-02 22:07:14 -0400

    add router.js and navbar component

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Hello from '@/components/Hello'
+
+Vue.use(Router)
+
+import home from './containers/home'
+import dashboard from './containers/dashboard'
+import projects from './containers/projects'
+
+
+// application routes
+const routes = [
+  { path: '/', component: home }, 
+  { path: '/dashboard', component: dashboard }, 
+  { path: '/projects', component: projects }
+]
+
+// export router instance 
+export default new Router({
+  mode: 'history', 
+  routes, 
+  linkActiveClass: 'is-active'
+})
